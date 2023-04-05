7e32edb499d62ad15c8bea9c33b12647061b147c
olawalethefirst <qbashy@gmail.com>
2023-04-05 23:08:11 +0100

    feat: defined basic types

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+import {attributeValueOptions} from './constants';
+
+export interface MachineCategory {
+  id: string;
+  name: string;
+  nameUnique: boolean;
+  lastUniqueName: string;
+  titleAttributeId: string;
+  attributes: MachineCategoryAttribute[];
+}
+
+export type AttributeValueOptions = (typeof attributeValueOptions)[number];
+
+export type AtrributeValue = string | number | boolean | null;
+
+export interface MachineCategoryAttribute {
+  id: string;
+  name: string;
+  nameUnique: boolean;
+  lastUniqueName: string;
+  valueOption: AttributeValueOptions;
+}
+
+export interface MachineAttribute {
+  id: string;
+  categoryAttributeId: string;
+  name: string;
+  value: AtrributeValue;
+  valueOption: AttributeValueOptions;
+}
+
+export interface Machine {
+  id: string;
+  categoryId: string;
+  attributes: MachineAttribute[];
+}
