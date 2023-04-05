c0d792d1382c1d1726d0b088866e2b9516ef1e07
olawalethefirst <qbashy@gmail.com>
2023-04-05 23:09:24 +0100

    feat: defined constants

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+// Machine Atrribute Value Types
+export const attributeValueOptions = [
+  'text',
+  'number',
+  'checkbox',
+  'date',
+] as const;
+
+export const colors = {
+  darkPurple: '88, 0, 232',
+  lightPurple: '114, 55, 204',
+  offWhiteBackground: '239, 239, 239',
+  white: '255, 255, 255',
+  lightGrey: '243, 243, 243',
+  darkGrey: '181, 181, 181',
+  black: '16, 16, 16',
+  red: '255, 0, 0',
+} as const;
