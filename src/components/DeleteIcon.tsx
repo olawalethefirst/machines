38b8204d27f5632c4a5faa9ac450f61fd74f63ea
olawalethefirst <qbashy@gmail.com>
2023-05-29 19:59:53 +0100

    feat: abstracted Delete icon

---
+++ src/components/DeleteIcon.tsx
@@ -0,0 +1,8 @@
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import color from '../utils/color';
+
+const DeleteIcon = () => (
+  <Icon name="delete" size={24} color={color('darkPurple')} />
+);
+
+export default DeleteIcon;
