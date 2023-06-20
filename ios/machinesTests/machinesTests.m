869f0d6219230a88038646a10f5d2876324738f7
olawalethefirst <qbashy@gmail.com>
2023-06-20 22:50:24 +0100

    feat: name change

--- ios/machinesTests/machinesTests.m
+++ ios/machinesTests/machinesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface webbeehackathonTests : XCTestCase
+@interface machinesTests : XCTestCase
 
 @end
 
-@implementation webbeehackathonTests
+@implementation machinesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
